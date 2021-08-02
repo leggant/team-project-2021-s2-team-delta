3d81ff2b0e1ba323844d5ebdd9e54a65f0dc63ef
scotms2 <53206496+scotms2@users.noreply.github.com>
2021-08-02 16:43:48 +1200

    Sprint 1 Login fix/features and js moved to seperate files (#56)
    * Reinstalled auth scaffold for project using jetstream, fortify and livewire. Moved models into a models folder to adhere to laravel 8 conventions
    
    * commented out the registration in fortify.php to remove the ability to register a new user
    
    * Changed it so login redirects to home page, changed login and logout button to display correct text. Added logout function to routes
    
    * Added middleware group in routes so that if the user isnt logged in they can't access them
    
    * added is_admin to users table, create admin panel view for admins use, made it so if user that is logged in isn't admin then they can't access admin panel
    
    * removed js from view files and put them into their own seperate js files. Tested that the search works, unsure if the js from viewStudent view worksas we don't currently access that page
    
    * added /home back to the routeserviceprovider file

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -17,8 +17,20 @@
                 <li class="nav-item">
                     <a href="{{route('pages.notes')}}" class="nav-link">Notes</a>
                 </li>
+                @if(Auth::check() && Auth::user()->isAdmin())
+                    <li class="nav-item">
+                        <a href="{{route('admin.admin-panel')}}">Admin Panel</a>
+                    </li>
+                @endif
                 <li class="nav-item" style="float:right">
-                    <a href="{{route('login')}}" class="nav-link">Log out</a>
+                    @if(Auth::check())
+                        <form method="POST" action="{{ route('logout') }}">
+                            {{csrf_field()}}
+                            <a href="{{route('logout')}}" class="nav-link">Log out</a>
+                        </form>
+                    @else
+                        <a href="{{route('login')}}" class="nav-link">Log in</a>
+                    @endif
                 </li>
             </ul>
         </div>
