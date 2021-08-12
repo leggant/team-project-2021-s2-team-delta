d625856b7522848f880a6c315d4220b927c5f378
Anthony Legg <leggtc1@student.op.ac.nz>
2021-08-12 22:10:39 +1200

    updated new student form to include student first/last names
    full name provided by the student model

--- resources/views/pages/addStudent.blade.php
+++ resources/views/pages/addStudent.blade.php
@@ -4,14 +4,16 @@
         <form action="{{ route('students.store') }}" method="post">
             @csrf
             <h1 class="title">Add Student</h1>
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" class="formInput" required>
+            <label for="first_name">First Name</label>
+            <input type="text" id="first_name" name="first_name" class="formInput" required>
+            <label for="last_name">Last Name</label>
+            <input type="text" id="last_name" name="last_name" class="formInput" required>
             <label for="id">Student ID:</label>
             <input type="text" id="id" name="student_id" class="formInput" required>
             <label for="mail">Email:</label>
             <input type="email" id="mail" name="email" class="formInput" required>
             <label for="Github">Github:</label>
-            <input type="text" id="github" name="github" class="formInput" required>
+            <input type="text" id="github" name="github" class="formInput">
             <button class="addBtn" type="submit">Save</button>
         </form>
     </div>
