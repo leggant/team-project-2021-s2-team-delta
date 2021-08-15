61c5dd82682e0c2f56bd0faff10ca1acf07ddf28
Anthony Legg <leggtc1@student.op.ac.nz>
2021-08-15 16:34:25 +1200

    Updated notes form page to use jetstream template #102 #93 #84

--- resources/views/pages/notes.blade.php
+++ resources/views/pages/notes.blade.php
@@ -1,24 +1,20 @@
-@extends('layouts.main-layout')
-@section('content')
-    <div class="testbox">
-        <form action="{{ route('pages.notes') }}" method="post">
-            @csrf
-            <h1>Notes</h1>
-            <p>Add note for a student</p>
-            <div>
-                <label for="student">Student: </label>
-                <input name="student" list="student" />
-                <datalist id="student">
-                    @foreach ($students as $student)
-                        <option value={{ $student->name }}>{{ $student->name }}</option>
-                    @endforeach
-                </datalist>
-            </div>
-            <h4>Notes:</h4>
-            <textarea rows="5" name="notes" required></textarea>
-            <div class="btn-block">
-                <button type="submit" name="addNotes" value="Add">Submit Note</button>
-            </div>
-        </form>
-    </div>
-@endsection
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold">Student Notes</h2>
+    </x-slot>
+    <form action="{{ route('pages.notes') }}" method="post">
+        @csrf
+        <div>
+            <label for="student">Student: </label>
+            <input name="student" list="student" />
+            <datalist id="student">
+                @foreach ($students as $student)
+                    <option value={{ $student->name }}>{{ $student->name }}</option>
+                @endforeach
+            </datalist>
+        </div>
+        <h4>Notes:</h4>
+        <textarea rows="5" name="notes" required></textarea>
+        <x-jet-button>Save Note</x-jet-button>
+    </form>
+</x-app-layout>
