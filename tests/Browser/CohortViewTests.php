a55ed8fb08589ba01565d26adbe24611fa58a814
revellgit <mrmattrevell@gmail.com>
2021-08-16 11:01:14 +1200

    Added blank browser test for cohort, commented out registration

---
+++ tests/Browser/CohortViewTests.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class CohortViewTests extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+}
