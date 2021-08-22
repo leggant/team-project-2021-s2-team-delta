1f5e36650786d6e770b8e6a3e0fa366f3f75528a
revellgit <mrmattrevell@gmail.com>
2021-08-23 11:45:37 +1200

    Sprint 2 qa cohort (#119)
    * Added blank browser test for cohort, commented out registration
    
    * Progress on browser tests for nav, cohort. Including login tests with user creation, and db:seed. Experiment with retaining login vs use DatabaseMigrations
    
    * Tests login, navtests, studentview working. Problems with serving dusk.local env, Made createuser function for each test file
    
    * Tests working every time, connected to local database
    
    * Modified createadminuser function to not create admin if already exists
    
    * Added cohort empty fields, overload tests
    
    * Added admin-panel to navtests, Added test for view cohort page
    
    * Building on login/out tests
    
    * Added comments to browser test files
    
    * Update resources/views/pages/addStudent.blade.php
    
    Co-authored-by: Anthony Legg <leggtc1@student.op.ac.nz>
    
    * Update resources/views/pages/addStudent.blade.php
    
    Co-authored-by: Anthony Legg <leggtc1@student.op.ac.nz>
    
    Co-authored-by: Anthony Legg <leggtc1@student.op.ac.nz>

--- tests/Browser/NavTest.php
+++ tests/Browser/NavTest.php
@@ -2,41 +2,63 @@
 
 namespace Tests\Browser;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class NavTest extends DuskTestCase
 {
-    use DatabaseMigrations;
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-
-     /* Commented out to keep for future reference of function layout
-
-        public function testExample()
-        {
-            $this->browse(function (Browser $browser) {
-                $browser->visit('/')
-                        ->assertSee('Laravel');
-            });
-        }
-    */
-
+    
     /* 
         Functions to test each link used in the navigation bar of the app
         i.e. /, /add-student, /cohort, /evidence, /notes, /login
         Each function visits the page and checks if a test word is present
         NB: Update the chrome-driver used for dusk with 'php artisan dusk:chrome-driver'
+    */   
+
+    // use DatabaseMigrations;
+
+    public function createadminuser()
+    {
+        $user = User::where('email', '=', 'admin@admin.com')->first();
+        if ($user === null) {
+            $this->user = User::factory()->create([
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password'),
+                'is_admin' => 1,
+            ]);
+        }
+    }    
+
+    /* 
+    Methods for testing each link on the navigation bar
+    to ensure the links point to the right places and the pages exist
     */
+    
+    public function testBypassLogin()
+    {
+        $this->createadminuser();
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1)) 
+                    ->visit('/')
+                    ->assertPathIs('/');
+                                       
+        });
+        
+    }
 
     public function testHomeLink()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
+            $browser
+                    ->visit('/')
+                    ->assertPathIs('/')
                     ->assertSee('Welcome');                    
         });
     }
@@ -73,11 +95,21 @@ class NavTest extends DuskTestCase
         });
     }
 
+    public function testAdminLink()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin-panel')
+                    ->assertSee('Manage');                    
+        });
+    }
+
     public function testLogoutLink()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/login')
-                    ->assertSee('Login');                    
+            $browser->visit('/')
+                    ->click('@loginout')
+                    ->assertPathIs('/login')
+                    ->assertSee('Password');                    
         });
     }
 }
