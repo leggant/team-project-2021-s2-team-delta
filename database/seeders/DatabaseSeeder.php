981f04e96752f27204ef69174a770b9c31779f1d
Anthony Legg <leggtc1@student.op.ac.nz>
2021-08-24 10:56:59 +1200

    correction to database seeder file-conflict merge error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        $this->call(UserSeeder::class);
-<<<<<<< HEAD
        $this->call(PapersSeeder::class);
-=======
->>>>>>> master
     }
 }
