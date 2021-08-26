523a979b37b8fc8e32f4c5d3d2f19b9b8fece8be
revellgit <mrmattrevell@gmail.com>
2021-08-26 19:22:44 +1200

    Added branch for students, evidence tests. Made test files. Added first basic test methods

---
+++ tests/Browser/StudentsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use database\factories\UserFactory;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Illuminate\Support\Facades\Hash;
+
+class StudentsTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    
+    public function createadminuser()
+    {
+        $user = User::where('email', '=', 'admin@admin.com')->first();
+        if ($user === null) {
+            $this->user = User::factory()->create([
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password'),
+                'is_admin' => 1,
+            ]);
+        }
+    }    
+    
+    public function testMakeStudent()
+    {
+        $this->createadminuser();
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/student')
+                    ->assertPathIs('/student')                   
+                    ;                    
+        });
+    }
+}
