8f0707f48a94f337df0431702c05fbb32721589c
Anthony Legg <leggtc1@student.op.ac.nz>
2021-08-30 02:09:18 +1200

    Update to dusk action #131
    discovered through testing a copy of the project that the database configuration was not correct. Have added some changes to the configuration files

--- config/database.php
+++ config/database.php
@@ -48,11 +48,27 @@ return [
         This 'dusk_testing' connection is referenced in the .env.dusk.local file
         */
 
-            'dusk_testing' => [
-                'driver' => 'sqlite',            
-                'database' => database_path('database.sqlite'),
-                'prefix' => '',            
-            ],
+        'dusktesting' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'dusktest'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql')
+                ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                ])
+                : [],
+        ],
 
         'mysql' => [
             'driver' => 'mysql',
