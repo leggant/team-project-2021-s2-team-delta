3a481e7bd68962c62bb8a5a0af580fe437ef1b74
revellgit <mrmattrevell@gmail.com>
2021-08-29 23:07:59 +1200

    Added id and dusk tags to inputs on evidence blade, Added createstudent method in evident test, Added simple create evidence test

--- tests/Browser/EvidenceTest.php
+++ tests/Browser/EvidenceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Browser;
 
 use App\Models\User;
+use App\Models\Student;
 use database\factories\UserFactory;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
@@ -31,15 +32,45 @@ class EvidenceTest extends DuskTestCase
         }
     }    
     
-    public function testCreateEvidence()
+    public function createstudent()
+    {
+        //$student = Student::where('email', '=', 'admin@admin.com')->first();
+        //if ($user === null) {
+            $this->student = Student::create([
+                
+                'first_name' => 'Jesus',
+                'last_name' => 'Christ',
+                'username' => 'HolyGhost',
+                'email' => 'jesus@hmail.com',
+                'github' => 'imagithub'
+            ]);
+        //}
+    }    
+
+    public function testEvidenceExists()
     {
         $this->createadminuser();
 
         $this->browse(function (Browser $browser) {
             $browser->loginAs(User::find(1))
                     ->visit('/evidence')
-                    ->assertPathIs('/evidence');
-                                        
+                    ->assertPathIs('/evidence');                                        
+        });
+    }
+
+    public function testCreateEvidence()
+    {
+        $this->createstudent();
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/evidence')
+                    ->type('@studentname', 'Jesus Christ')
+                    ->type('#title', 'Some Evidence')
+                    //->press('@image')
+                    //->choose dummy file ???
+                    ->click('button[type="submit"]')
+                    ->assertTitle('Studio Management');                                
         });
     }
 }
