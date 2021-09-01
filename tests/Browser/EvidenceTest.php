cd067ed0c26a6a616402427e8d037c1297dfab71
revellgit <mrmattrevell@gmail.com>
2021-09-01 23:06:53 +1200

    Tweaked student names in dropdown for evidence view, Commented test files, Added dusk attach( ) test for file uploads

--- tests/Browser/EvidenceTest.php
+++ tests/Browser/EvidenceTest.php
@@ -32,12 +32,17 @@ class EvidenceTest extends DuskTestCase
         }
     }    
     
+    /* 
+    Method to create a new student need to test evidence uploads
+    Used to add the student directly to the database rather than
+    via the students page
+    */
+
     public function createstudent()
     {
         //$student = Student::where('email', '=', 'admin@admin.com')->first();
         //if ($user === null) {
-            $this->student = Student::create([
-                
+            $this->student = Student::create([                
                 'first_name' => 'Jesus',
                 'last_name' => 'Christ',
                 'username' => 'HolyGhost',
@@ -58,6 +63,13 @@ class EvidenceTest extends DuskTestCase
         });
     }
 
+    /* 
+    Attempt to upload a file using a full student name (first, last)
+    and a file stored in 'app/public/testing/'
+    ---Currently failing due changes to name field to first,last_name
+    (unknown column 'name')
+    */
+
     public function testCreateEvidence()
     {
         $this->createstudent();
@@ -66,9 +78,9 @@ class EvidenceTest extends DuskTestCase
             $browser->loginAs(User::find(1))
                     ->visit('/evidence')
                     ->type('@studentname', 'Jesus Christ')
+                    // ->select('student', '1') Choose the first name in the dropdown
                     ->type('#title', 'Some Evidence')
-                    //->press('@image')
-                    //->choose dummy file ???
+                    ->attach('@image', storage_path('app/public/testing/test_upload.png'))
                     ->click('button[type="submit"]')
                     ->assertTitle('Studio Management');                                
         });
