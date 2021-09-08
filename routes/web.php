0ccdfadcd15ce96b93f53acee5eaa09c30d3fbc4
Anthony Legg <leggtc1@student.op.ac.nz>
2021-09-08 15:44:07 +1200

    update web route and route service provider

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\ {
+    StudentController,
+    UserController,
+    CohortController,
+    EvidenceController,
+    NoteController,
+    PermissionController,
+    RoleController,
+}; 
 
 /*
 |--------------------------------------------------------------------------
