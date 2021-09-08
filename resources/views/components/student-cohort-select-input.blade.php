541cf488d6249e2f76c0de3400a5f9a433513584
Anthony Legg <leggtc1@student.op.ac.nz>
2021-09-08 17:45:42 +1200

    updated student/cohort select, now pulling data from cohort
    updated student page to utilise more view port space available also

--- resources/views/components/student-cohort-select-input.blade.php
+++ resources/views/components/student-cohort-select-input.blade.php
@@ -1,13 +1,11 @@
-<div class="my-4">
-    <select id="cohort" name="cohorts" required
-        class="focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 appearance-none bg-white border border-gray-400
-        hover:border-gray-500 px-4 py-2 pr-8 leading-tight
-        focus:outline-none focus:shadow-outline w-1/2">
-        <option disabled selected>Pick a Cohort for the Student</option>
-        @foreach ($cohorts as $cohort)
-            <option value="{{ $cohort->id }}">Studio {{ $cohort->paper_id }} | {{ $cohort->year }} |
-                {{ $cohort->semester }}
-            </option>
-        @endforeach
-    </select>
-</div>
+<select id="cohort" name="cohorts" required
+    class="focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block appearance-none bg-white border border-gray-400
+    hover:border-gray-500 px-4 py-2 pr-8 leading-tight
+    focus:outline-none focus:shadow-outline col-span-2">
+    <option disabled selected>Pick a Cohort for the Student</option>
+    @foreach ($cohorts as $cohort)
+        <option value="{{ $cohort->id }}">Studio {{ $cohort->paper_id }} | {{ $cohort->year }} |
+            {{ $cohort->semester }}
+        </option>
+    @endforeach
+</select>
--- resources/views/components/student-list.blade.php
+++ resources/views/components/student-list.blade.php
@@ -1,23 +1,20 @@
-<div>
-    <input type="text" id="myInput" onkeyup="search()" placeholder="Search for names..">
+    <input type="text" id="myInput" onkeyup="search()" placeholder="Search for names.." class="w-1/2">
     <table cellspacing="0" id="studentTable" class="table-fixed w-full">
-        <tr>
             <th class="px-4 py-2" onclick="sortTable(0)">Student Name</th>
-            <th class="px-4 py-2" onclick="sortTable(1)">Username</th>
             <th class="px-8 py-2" onclick="sortTable(2)">Email</th>
             <th class="px-4 py-2" onclick="sortTable(3)">Github</th>
             <th class="px-4 py-2" onclick="sortTable(4)">Cohort + Paper</th>
         </tr>
         @foreach ($students as $student)
             <tr>
-                <td class="border px-4 py-2"><a id="studentLink"
+                <td class="border px-4 py-2 cursor-pointer">
+                    <a id="studentLink"
                         href="{{ route('students.show', $student) }}">{{ $student->name }}
-                </td></a>
-                <td class="border px-4 py-2">{{ $student->username }}</td>
+                    </a>
+                </td>
                 <td class="border px-8 py-2">{{ $student->email }}</td>
                 <td class="border px-4 py-2">{{ $student->github }}</td>
                 <td class="border px-4 py-2">Studio {{ $student->cohort->paper_id }} | {{ $student->cohort->year }} | {{ $student->cohort->semester }}</td>
             </tr>
         @endforeach
     </table>
-</div>
