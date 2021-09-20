f3924d3038f11ab1530d8c16cc56827a7fdd677a
unknown <scotms2@student.op.ac.nz>
2021-09-20 15:55:41 +1200

    created seeder to add studio roles

---
+++ database/migrations/2021_09_20_031301_assign_roles_to_users.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class AssignRolesToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $users = User::all();
+        foreach($users as $u) {
+            if($u->email == 'admin@admin.com')
+            {
+                $u->assignRole('Super-Admin');
+            }
+            else {
+                foreach($u->papers as $up)
+                {
+                    if($up->paper_name == "Studio 1")
+                    {
+                        $u->assignRole('Studio 1');
+                    }
+                    elseif($up->paper_name == "Studio 2")
+                    {
+                        $u->assignRole('Studio 2');
+                    }
+                    elseif($up->paper_name == "Studio 3")
+                    {
+                        $u->assignRole('Studio 3');
+                    }
+                    elseif($up->paper_name == "Studio 4") 
+                    {
+                        $u->assignRole('Studio 4');
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
