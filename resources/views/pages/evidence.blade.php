67445afea9affbcaca954c98d6abd277cb6b8748
Anthony Legg <leggtc1@student.op.ac.nz>
2021-09-24 21:23:26 +1200

    evidence form update, layout #127

--- resources/views/pages/evidence.blade.php
+++ resources/views/pages/evidence.blade.php
@@ -3,23 +3,30 @@
         <x-slot name="header">
             <h1 class="font-semibold">Student Evidence Upload</h1>
         </x-slot>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 mt-8">
             <form action="{{ route('evidence.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
-                <label for="student">Student</label>
-                <select id="student" name="student">
-                    <option disabled selected>Select a student</option>
-                    @foreach ($student as $s)
-                        <option value="{{ $s->id }}">{{ $s->first_name }} {{ $s->last_name }}</option>
-                    @endforeach
-                </select>
-                <label for="title">Title</label>
-                <input type="text" name="title" id="title" placeholder="Enter Title" required>
-                <textarea name="description" id="description" cols="30" rows="5"></textarea>
-                {{-- MULTIPLE ALLOWS MULTIPLE FILES TO BE UPLOADED. CHECK BACKEND CAN HANDLE THIS IN THE HTTP REQUEST --}}
-                <input type="file" name="filepath" id="image" dusk="image" accept="image/*,.pdf,.doc,.docx,.md,.html" multiple
-                    required />
-                <x-jet-button>Upload Files</x-jet-button>
+                <div class="bg-white overflow-hidden shadow rounded-lg grid grid-cols-2 gap-6 py-4 px-4 drop-shadow-2xl">
+                    <x-jet-label for="student" class="">Student</x-jet-label>
+                    <select id="student" name="student"
+                        class='border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm'
+                        required>
+                        <option value="ignore">Select Student</option>
+                        @foreach ($student as $s)
+                            <option value="{{ $s->id }}"
+                                class='border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm'>
+                                {{ $s->first_name }} {{ $s->last_name }}</option>
+                        @endforeach
+                    </select>
+                    <x-jet-input type="text" name="title" id="title" placeholder="Enter Title" required aria-label="Upload Title" aria-placeholder="Enter A Upload Title" placeholder="Enter A Upload Title"></x-jet-input>
+                    <x-jet-label for="description">Description</x-jet-label>
+                    <textarea name="description" id="description" cols="30" rows="5" placeholder="Optional Description, Context"></textarea>
+                    {{-- MULTIPLE ALLOWS MULTIPLE FILES TO BE UPLOADED. CHECK BACKEND CAN HANDLE THIS IN THE HTTP REQUEST --}}
+                    <x-jet-label for="filepath">Select File(s)</x-jet-label>
+                    <x-jet-input type="file" name="filepath" id="image" dusk="image" accept="image/*,.pdf,.doc,.docx,.md,.html"
+                        multiple required class='border-indigo-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-xl py-2 px-2 my-2'></x-jet-input>
+                    <x-jet-button>Upload Files</x-jet-button>
+                </div>
             </form>
         </div>
     @else
