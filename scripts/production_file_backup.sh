8017e3caebd3a76522fbb93d38293e5864dbd1fa
s-winton <WINTST1@student.op.ac.nz>
2021-09-25 12:21:50 +1200

    outputted script actions to log file

--- scripts/production_file_backup.sh
+++ scripts/production_file_backup.sh
@@ -5,5 +5,21 @@ currentDate=$(date +"%d-%m-%Y")
 path=/home/backerupper/backup/productionFiles
 log=/home/backerupper/backup/production_file_backup_log.txt
 
+echo -e "\n Script executed on $(date)" >> $log
 
-aws s3 sync s3://studio4managementfiles2021 .
+mkdir -p $path
+if [ $? -ne 0 ]; then
+        echo "Production capture directory not created. Error code $?. Exiting..." >> $log
+        exit 0
+fi
+
+echo "Beginning file downloads..." >> $log
+
+aws s3 sync s3://studio4managementfiles2021 $path >> $log
+
+if [ $? -ne 0 ]; then
+        echo "Download failed. Error code $?. Exiting..." >> $log
+        exit 0
+else
+        echo "Download completed." >> $log
+fi
