1e6cf7843023267e30ee5773b5821e6a42452194
Anthony Legg <leggtc1@student.op.ac.nz>
2021-10-19 19:56:47 +1300

    style(homepage css change): update added to student list component
    change present tables more consistently with content in other pages
    
    #181, #224

--- resources/views/components/student-list.blade.php
+++ resources/views/components/student-list.blade.php
@@ -1,8 +1,8 @@
 @can('view students')
-    <div>
-        @foreach ($user->papers as $up)
-            <table cellspacing="0" id="studentTable" class="table-fixed w-full">
-                <h1 class="text-center py-4">{{ $up->paper_name }}</h1>
+    @foreach ($user->papers as $up)
+        <div class="bg-white shadow rounded-lg py-6 px-4 drop-shadow-2xl mx-auto w-3/4 grid mb-6">
+            <h1 class="text-center text-4xl mb-4">{{ $up->paper_name }}</h1>
+            <table id="studentTable">
                 <tr>
                     <th class="px-4 py-2">Student Name</th>
                     <th class="px-8 py-2">Email</th>
@@ -12,26 +12,32 @@
                 @foreach ($students as $student)
                     @if ($up->pivot->paper_id == $student->cohort->paper_id)
                         <tr>
-                            <td class="border px-4 py-2 cursor-pointer">
-                                <a id="studentLink" href="{{ route('students.show', $student) }}">{{ $student->name }}
+                            <td class="border py-2 cursor-pointer text-center">
+                                <a class="hover:underline"
+                                    href="{{ route('students.show', $student) }}">{{ $student->name }}
                                 </a>
                             </td>
-                            <td class="border px-8 py-2"><a class="hover:underline"
-                                    href="mailto:{{ $student->username }}@student.op.ac.nz">{{ $student->username }}@student.op.ac.nz</a>
+                            <td class="border py-2 text-center">
+                                <a class="hover:underline"
+                                    href="mailto:{{ $student->username }}@student.op.ac.nz">{{ $student->username }}@student.op.ac.nz
+                                </a>
                             </td>
                             @if ($student->github)
-                                <td class="border px-4 py-2"><a class="hover:underline"
-                                        href="http://github.com/{{ $student->github }}"
+                                <td class="border py-2 text-center">
+                                    <a class="hover:underline" href="http://github.com/{{ $student->github }}"
                                         target="_blank">github.com/{{ $student->github }}
+                                    </a>
                                 </td>
                             @endif
-                            <td class="border px-4 py-2">{{ $student->cohort->papers->paper_name }} | Stream
-                                {{ $student->cohort->stream }}</td>
+                            <td class="border py-2 text-center">
+                                {{ $student->cohort->papers->paper_name }} | Stream
+                                {{ $student->cohort->stream }}
+                            </td>
                         </tr>
                     @endif
                     </tr>
                 @endforeach
-        @endforeach
-        </table>
-    </div>
+            </table>
+        </div>
+    @endforeach
 @endcan
