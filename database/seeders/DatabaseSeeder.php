1b25903414bb52387d825454c321c2c3045fd5a8
Anthony Legg <leggtc1@student.op.ac.nz>
2021-10-21 22:53:51 +1300

    fix(databaseseeder.php): correcting error in seeder order
    #232

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(StudioRoleSeeder::class);
         $this->call(PermissionsSeeder::class);
+        $this->call(StudioRoleSeeder::class);
         $this->call(PapersSeeder::class);
         $this->call(LecturerRoleSeeder::class);
     }
