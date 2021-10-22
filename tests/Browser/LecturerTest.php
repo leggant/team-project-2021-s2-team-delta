77562b8cd70f25da6e5f8cc13efa5d31198aa6e7
Anthony Legg <leggtc1@student.op.ac.nz>
2021-10-22 14:29:26 +1300

    New test file for lecturer access, first test func

---
+++ tests/Browser/LecturerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\User;
+
+class LecturerTest extends DuskTestCase
+{
+    public function testFindAdminPage()
+    {       
+        $user = User::where('is_admin', 1)->first();
+
+        $this->browse(function ($browser) use($user) {
+            $browser->loginAs($user)
+                    ->visit('/admin-panel')
+                    ->pause(2000)
+                    ->assertPathIs('/admin-panel')                   
+                    ->assertSee('MANAGE USERS');                    
+        });
+    }
+}
