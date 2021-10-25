caba33c5c6765ee45871a3587534c2cee87407e5
Anthony Legg <leggtc1@student.op.ac.nz>
2021-10-26 11:47:50 +1300

    test(navtest.php): test errors have been resolved, 100% success rate
    it did have a number of separate tests, one for each route, which required a login before clicking a
    single link. Having multiple tests running from the same file caused a error "To Many Requests" -
    combining all tests into a single method resolved this issue, also added ids to the nav bar links so
    that these could be accessed by the tests
    
    #235, #229, #199

--- tests/Browser/NavTest.php
+++ tests/Browser/NavTest.php
@@ -7,15 +7,14 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Chrome;
 use App\Models\User;
 
-
 class NavTest extends DuskTestCase
 {    
-    /* 
+    /*
         Functions to test each link used in the navigation bar of the app
         i.e. /, /add-student, /cohort, /evidence, /notes, /login
         Each function visits the page and checks if a test word is present
         NB: Update the chrome-driver used for dusk with 'php artisan dusk:chrome-driver'
-    */   
+    */
     public function setUp(): void
     {
         parent::setUp();
@@ -31,10 +30,10 @@ class NavTest extends DuskTestCase
     public function testBypassLogin()
     {
         $user = User::where('name', 'Administrator')->get();
-        $this->browse(function ($browser1) use($user)
+        $this->browse(function ($browser) use($user)
         {
             $email = $user[0]->email;
-            $browser1
+            $browser
                 ->visit('/login')
                 ->assertPathIs('/login')
                 ->value('#email', $email)
@@ -42,152 +41,73 @@ class NavTest extends DuskTestCase
                 ->click('#login')
                 ->visit('/')
                 ->assertPathIs('/')
-                ->pause(2000);                                   
-        });
-    }
-
-    public function testHomeLink()
-    {
-        $user = User::where('name', 'Administrator')->get();
-        $this->browse(function ($browser2) use($user)
-        {
-            $email = $user[0]->email;
-            $browser2
-                ->visit('/login')
-                ->assertPathIs('/login')
-                ->value('#email', $email)
-                ->type('#password', 'studio2021')
-                ->click('#login')
-                ->visit('/')
-                ->pause(2000)
-                ->screenshot('HOMELINK_home')
-                ->assertPathIs('/')
-                ->assertSee('Admin')
-                ->pause(2000);          
-        });
-    }
-
-    public function testNewStudentLink()
-    {
-        $user = User::where('name', 'Administrator')->get();
-        $this->browse(function ($browser3) use($user)
-        {
-            $email = $user[0]->email;
-            $browser3
-                ->visit('/login')
-                ->assertPathIs('/login')
-                ->value('#email', $email)
-                ->type('#password', 'studio2021')
-                ->click('#login')
-                ->pause(2000)
-                ->assertPathIs('/')
-                ->assertSee('Student Admin')
-                ->click('#students')
-                ->pause(2000)
-                ->screenshot('STUDENT_page')
                 ->pause(2000);
         });
     }
 
-    public function testEvidenceLink()
+    public function testNavLinks()
     {
         $user = User::where('name', 'Administrator')->get();
-        $this->browse(function ($browser4) use($user)
+        $this->browse(function ($browser) use($user)
         {
             $email = $user[0]->email;
-            $browser4
+            $browser
                 ->visit('/login')
                 ->assertPathIs('/login')
                 ->value('#email', $email)
                 ->type('#password', 'studio2021')
                 ->click('#login')
+                # Home Page
+                ->visit('/')
                 ->pause(2000)
-                ->screenshot('EVIDENCE_ERROR')
-                ->assertSee('Uploads')
+                ->screenshot('HOMELINK_home')
+                ->assertPathIs('/')
+                ->assertSee('Student Admin')
+                ->pause(2000)
+                # student page
+                ->click('#students')
+                ->pause(1500)
+                // ->assertPathIs('/students')
+                ->assertSee('Welcome Administrator')
+                ->screenshot('STUDENT_page')
+                ->pause(1500)
+                # evidence page
                 ->click('#evidence')
                 ->pause(1500)
                 ->assertPathIs('/evidence')
                 ->screenshot('EVIDENCE_LINK')
-                ->pause(2000);   
+                ->pause(1500)
+                # notes page
+                ->click('#notes')
+                ->pause(1500)
+                ->assertPathIs('/notes')
+                ->screenshot('Notes_page')
+                ->assertSee('SAVE NOTE')
+                ->pause(1500)
+                # cohort page
+                ->click('#cohorts')
+                ->assertPathIs('/cohorts')
+                ->screenshot('Cohorts_page')
+                ->assertSee('Studio Cohorts')
+                ->pause(1500)
+                # admin page
+                ->click('#admin')
+                ->pause(1500)
+                ->assertPathIs('/users')
+                ->screenshot('Admin_page')
+                ->assertSee('Current Registered Users')
+                #home page
+                ->click('#home')
+                ->pause(1500)
+                ->assertPathIs('/')
+                ->screenshot('Home_page')
+                ->pause(1500)
+                #logout
+                ->click('#logout')
+                ->pause(1500)
+                ->assertPathIs('/login')
+                ->assertSee('Password')
+                ->assertSee('Email');
         });
     }
-
-    // public function testNotesLink()
-    // {
-    //     $user = User::where('name', 'Administrator')->get();
-    //     $this->browse(function ($browser) use($user)
-    //     {
-    //         $email = $user[0]->email;
-    //         $browser
-    //             ->visit('/login')
-    //             ->assertPathIs('/login')
-    //             ->value('#email', $email)
-    //             ->type('#password', 'studio2021')
-    //             ->click('#login')
-    //             ->visit('/notes')
-    //             ->assertPathIs('/notes')
-    //             ->assertSee('SAVE NOTE');                    
-    //     });
-    // }
-
-    // public function testCohortLink()
-    // {
-    //     $user = User::where('name', 'Administrator')->get();
-    //     $this->browse(function ($browser) use($user)
-    //     {
-    //         $email = $user[0]->email;
-    //         $browser
-    //             ->visit('/login')
-    //             ->assertPathIs('/login')
-    //             ->value('#email', $email)
-    //             ->type('#password', 'studio2021')
-    //             ->click('#login') 
-    //             ->visit('/cohorts')
-    //             ->assertPathIs('/cohorts')
-    //             ->assertSee('Studio Cohorts');                    
-    //     });
-    // }    
-    
-    // public function testAdminLink()
-    // {
-    //     $user = User::where('name', 'Administrator')->get();
-    //     $this->browse(function ($browser) use($user)
-    //     {
-    //         $email = $user[0]->email;
-    //         $browser
-    //             ->visit('/login')
-    //             ->assertPathIs('/login')
-    //             ->value('#email', $email)
-    //             ->type('#password', 'studio2021')
-    //             ->click('#login') 
-    //             ->visit('/admin-panel')
-    //             ->pause(1500)
-    //             ->assertPathIs('/admin-panel')
-    //             ->assertSee('MANAGE');                    
-    //     });
-    // }    
-
-    // public function testLogoutLink()
-    // {
-    //     $user = User::where('name', 'Administrator')->get();
-    //     $this->browse(function ($browser) use($user)
-    //     {
-    //         $email = $user[0]->email;
-    //         $browser
-    //             ->visit('/login')
-    //             ->assertPathIs('/login')
-    //             ->value('#email', $email)
-    //             ->type('#password', 'studio2021')
-    //             ->click('#login')
-    //             ->pause(2000)
-    //             ->waitUntilMissing('#login')
-    //             ->screenshot('LOGOUTLINK_home')
-    //             ->assertSee('Student Admin')
-    //             ->click('#logout')
-    //             ->pause(1000)
-    //             ->assertPathIs('/login')
-    //             ->assertSee('password')
-    //             ->assertSee('email');                  
-    //     });
-    // }
 }
