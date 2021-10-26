a96282633526104760e1c3299784df8c4285869e
revellgit <mrmattrevell@gmail.com>
2021-10-26 13:57:42 +1300

    Making changes and additions to adminpanel/user tests

--- tests/Browser/AdminpanelTest.php
+++ tests/Browser/AdminpanelTest.php
@@ -11,23 +11,50 @@ class AdminpanelTest extends DuskTestCase
 {
     public function testFindAdminPage()
     {       
-        $user = User::where('email', 'admin@admin.com')->first();
+        $user = User::where('is_admin', 1)->first();
 
         $this->browse(function ($browser) use($user) {
             $browser->loginAs($user)
-                    ->visit('/admin-panel')
+                    ->visit('/users')
                     ->pause(2000)
-                    ->assertPathIs('/admin-panel')                   
-                    ->assertSee('MANAGE USERS');                    
+                    ->assertPathIs('/users')                   
+                    ->assertSee('Current Registered Users');                    
         });
     }
+
+    // Method to test user creation for admin
+    // involves navigation to pages and inputting into several fields
+    // then confirming the operation was successful
+
+    public function testCreateNewAdminUser()
+    {    
+        $this->browse(function ($browser) {
+            $browser->visit('/users')
+                    ->pause(2000)
+                    ->assertPathIs('/users')                   
+                    ->assertSee('Current Registered Users')
+                    ->press('CREATE NEW USER')
+                    ->assertPathIs('/users/create*')
+                    ->screenshot('usercreatescreen')
+                    ->type('#Name', 'Dusk Admin')
+                    ->type('#Email', 'fake@fakeemail.com')
+                    ->type('#Password', 'SeriouslyComplexPassword987')
+                    ->check('#Admin')
+                    ->select('Papers[]', 2)
+                    ->screenshot('createadmininputs')
+                    ->press('SUBMIT')
+                    ;                    
+        });
+    }
+
+    /* Old tests designed for functionality that no-longer exists
+    i.e. links have changed and method for creating, editing users is now different
 
     public function testManageUsers()
     {    
         $this->browse(function ($browser) 
         {
-            $browser
-                    ->visit('/admin-panel')
+            $browser->visit('/admin-panel')
                     ->pause(2000)
                     ->assertPathIs('/admin-panel')                   
                     ->press('MANAGE USERS')
@@ -78,6 +105,6 @@ class AdminpanelTest extends DuskTestCase
                     ->assertPathIs('/roles');                    
         });
     }
-
+    */
 
 }
