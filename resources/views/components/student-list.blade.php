8f87097931cee08d5896278b3af53b7579fed0a8
Anthony Legg <leggtc1@student.op.ac.nz>
2021-10-30 16:32:30 +1300

    fix(updated student.blade.php): bug fix added to resolve blade `@if` statement
    output on null values no longer returns errors
    
    PR #241, #179

--- resources/views/components/student-list.blade.php
+++ resources/views/components/student-list.blade.php
@@ -1,57 +1,58 @@
 @can('view students')
-<div class="grid w-full 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 mb-6 mx-auto gap-x-6 gap-y-6 sm:w-full grid-flow-row justify-items-stretch items-stretch">
-@foreach ($cohorts as $cohort)
-        @foreach ($user->papers as $up)
-            @if ($up->pivot->paper_id == $cohort->paper_id)
-                <div class="bg-white shadow rounded-lg py-6 px-4 drop-shadow-2xl content-start h-content lg:px-4">
-                    <h1 class="text-center text-4xl mb-4">{{ $up->paper_name }} -
-                        {{ date('Y', strtotime($cohort->year)) }} Stream
-                        {{ $cohort->stream }}
-                    </h1>
-                    <table id="studentTable" class="border-collapse w-full">
-                        <thead>
-                            <tr>
-                                <th class="py-2 px-2 text-center lg:whitespace-nowrap md:whitespace-nowrap">Student Name</th>
-                                <th class="py-2 px-2 text-center">Email</th>
-                                <th class="py-2 px-2 text-center">Github</th>
-                                <th class="py-2 px-2 text-center"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($students as $student)
-                                @if ($up->pivot->paper_id == $student->cohort->paper_id)
-                                    <tr class="h-min-1">
-                                        <td class="border py-2 px-2 text-center">
-                                            <a class="hover:underline"
-                                                href="mailto:{{ $student->username }}@student.op.ac.nz">{{ $student->name }}
-                                            </a>
-                                        </td>
-                                        <td class="border py-2 px-2 text-center">
-                                            <a class="hover:underline"
-                                                href="mailto:{{ $student->username }}@student.op.ac.nz">{{ $student->username }}@student.op.ac.nz
-                                            </a>
-                                        </td>
-                                        <td class="border py-2 px-2 text-center">
-                                        @if ($student->github)
-                                            <a class="hover:underline" href="http://github.com/{{ $student->github }}"
-                                                    target="_blank">github.com/{{ $student->github }}
-                                            </a>
-                                        @endif
-                                        </td>
-                                        <td class="border py-2 px-2 text-center">
-                                            <form action="{{ route('students.show', $student) }}" method="GET">
-                                                @csrf
-                                                <x-jet-button>Profile</x-jet-button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endif
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            @endif
-        @endforeach
-        @endforeach
+<div
+    class="grid w-full 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 mb-6 mx-auto gap-x-6 gap-y-6 sm:w-full grid-flow-row justify-items-stretch items-stretch">
+    @foreach ($cohorts as $cohort)
+    @foreach ($user->papers as $up)
+    @if ($up->pivot->paper_id == $cohort->paper_id)
+    <div class="bg-white shadow rounded-lg py-6 px-4 drop-shadow-2xl content-start h-content lg:px-4">
+        <h1 class="text-center text-4xl mb-4">{{ $up->paper_name }} -
+            {{ date('Y', strtotime($cohort->year)) }} Stream
+            {{ $cohort->stream }}
+        </h1>
+        <table id="studentTable" class="border-collapse w-full">
+            <thead>
+                <tr>
+                    <th class="py-2 px-2 text-center lg:whitespace-nowrap md:whitespace-nowrap">Student Name</th>
+                    <th class="py-2 px-2 text-center">Email</th>
+                    <th class="py-2 px-2 text-center">Github</th>
+                    <th class="py-2 px-2 text-center"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($students as $student)
+                @if ($up->pivot->paper_id == $student->cohort->paper_id)
+                <tr class="h-min-1">
+                    <td class="border py-2 px-2 text-center">
+                        <a class="hover:underline" href="mailto:{{ $student->username }}@student.op.ac.nz">{{
+                            $student->name }}
+                        </a>
+                    </td>
+                    <td class="border py-2 px-2 text-center">
+                        <a class="hover:underline" href="mailto:{{ $student->username }}@student.op.ac.nz">{{
+                            $student->username }}@student.op.ac.nz
+                        </a>
+                    </td>
+                    <td class="border py-2 px-2 text-center">
+                        @if ($student->github)
+                        <a class="hover:underline" href="http://github.com/{{ $student->github }}"
+                            target="_blank">github.com/{{ $student->github }}
+                        </a>
+                        @endif
+                    </td>
+                    <td class="border py-2 px-2 text-center">
+                        <form action="{{ route('students.show', $student) }}" method="GET">
+                            @csrf
+                            <x-jet-button>Profile</x-jet-button>
+                        </form>
+                    </td>
+                </tr>
+                @endif
+                @endforeach
+            </tbody>
+        </table>
     </div>
+    @endif
+    @endforeach
+    @endforeach
+</div>
 @endcan
--- resources/views/pages/students.blade.php
+++ resources/views/pages/students.blade.php
@@ -1,22 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
-        <h1 class="font-semibold capitalize">Welcome {{ Str::title($user->name) }} - Student Admin</h1>
+        <h1 class="font-semibold capitalize">Welcome {{ Str::title($user->name) }} - BIT Student Admin</h1>
     </x-slot>
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 grid grid-rows-1 gap-4 mt-8 grid-flow-row">
-        <x-student-form :cohorts="$cohorts" :students="$students" :user="$user"/>
+        <x-student-form :cohorts="$cohorts" :students="$students" :user="$user" />
     </div>
     <div class="mx-auto sm:px-6 lg:px-8 grid w-full grid-rows-1 gap-4 mt-8">
         @if ($students->count() >= 1 && $cohorts->count() >= 1)
-            <x-student-list :students="$students" :cohorts="$cohorts" :user="$user"/>
+            <x-student-list :students="$students" :cohorts="$cohorts" :user="$user" />
         @endif
-        @if ($cohorts->isEmpty() && $students->isEmpty())
-            <h2 class="text-center text-3xl">There Are No Papers With Students Currently Assigned To {{ Str::title($user->name) }}</h2>
-        @elseif ($students->isEmpty())
-            <h2 class="text-center text-3xl">There Are No Students Currently Assigned To {{ Str::title($user->name) }}</h2>
-        @elseif ($cohorts->isEmpty())
-            <h2 class="text-center text-3xl">There Are No Papers Currently Assigned To {{ Str::title($user->name) }}</h2>
-        @elseif ($user->papers[0]->id == 1)
-            <h2 class="text-center text-3xl">There Are No Students Currently Assigned To {{ Str::title($user->name) }}</h2>
-        @endif
+        @foreach ($user->papers as $paper)
+            @if ($paper->id == 1)
+                <h2 class="text-center text-3xl">There Are No Papers Currently Assigned To
+                    {{ Str::title($user->name) }}
+                </h2>
+            @endif
+            @foreach ($cohorts as $cohort)
+                @if (count($students) == 0)
+                    <h2 class="text-center text-3xl">
+                        There Are No Students Currently Assigned To {{ Str::title($user->name) }}
+                    </h2>
+                @endif
+                @once
+                    @if ($cohort->students == null && $paper->id != 1 || $cohorts->count() <= 0)
+                        <h2 class="text-center text-3xl">
+                            {{$paper->paper_name}} Has Been Assigned To {{ Str::title($user->name) }} But Has No Students Currently 
+                        </h2>
+                    @endif
+                @endonce
+            @endforeach
+        @endforeach
     </div>
 </x-app-layout>
