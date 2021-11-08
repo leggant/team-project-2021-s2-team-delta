a29d9f6135e3e02be6b5b8c9c8cea45136339e27
Anthony Legg <leggtc1@student.op.ac.nz>
2021-11-08 23:35:29 +1300

    refactor(user admin student user list update): refactored the columns for more space
    combined student user name, and email into one col making the student username a clickable link to
    open a email or right click and copy the student email address to the clipboard.

--- resources/views/pages/studentListView.blade.php
+++ resources/views/pages/studentListView.blade.php
@@ -6,7 +6,7 @@
     <div class="xl:max-w-full mx-auto md:px-4 lg:px-8 grid 2xl:grid-cols-2 2xl:max-w-full xl:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-x-8 mt-8 pb-6">
         @foreach ($groups as $group)
         <div
-            class="bg-white shadow rounded-lg py-6 drop-shadow-2xl w-full mb-6 mx-auto sm:px-6 lg:px-8 grid mt-8 grid-flow-row content-start max-h-min">
+            class="bg-white shadow rounded-lg py-6 drop-shadow-2xl w-full mb-6 mx-auto sm:px-6 content-start max-h-min">
             @if ($loop->first)
             @once
             <h2 class="text-center text-3xl font-bold mb-4">All Active Students</h2>
@@ -14,9 +14,8 @@
                 <thead>
                     <tr>
                         <th class="md:px-4 py-2">Name</th>
-                        <th class="md:px-4 py-2">Username</th>
+                        <th class="md:px-4 py-2">Username/Email</th>
                         <th class="md:px-4 py-2">Github</th>
-                        <th class="md:px-4 py-2">Email</th>
                         <th class="md:px-4 py-2">Active?</th>
                         <th class="md:px-4 py-2">Current Class</th>
                     </tr>
@@ -31,14 +30,13 @@
                                     }}</button>
                             </form>
                         </td>
-                        <td class="border md:py-2 px-2 text-center">{{$s->username}}</td>
+                        <td class="border md:py-2 px-2 text-center hover:underline">
+                            <a href="mailto:{{$s->email}}" class="hover:underline">{{$s->username}}</a>
+                        </td>
                         <td class="border md:py-2 px-2 text-center">
                             <a class="hover:underline hover:cursor-pointer"
                                 href="http://www.github.com/{{ $s->github }}" target="_blank">{{ $s->github }}
                             </a>
-                        </td>
-                        <td class="border md:py-2 px-2 text-center hover:underline">
-                            <a href="mailto:{{$s->email}}" class="hover:underline">{{$s->email}}</a>
                         </td>
                         <td class="border md:py-2 px-2 text-center">
                             @if($s->is_active == 1)
@@ -70,8 +68,7 @@
                     <tr>
                         <th class="md:px-4 py-2"></th>
                         <th class="md:px-4 py-2">Name</th>
-                        <th class="md:px-4 py-2">Username</th>
-                        <th class="md:px-4 py-2">Email</th>
+                        <th class="md:px-4 py-2">Username/Email</th>
                         <th class="md:px-4 py-2">Github</th>
                         <th class="md:px-4 py-2">Active?</th>
                     </tr>
@@ -80,15 +77,21 @@
                         <td class="border py-2 px-2 text-center"><input type="checkbox" name="student_checkboxes[]"
                                 value="{{$s->id}}" id="checkbox_students">
                         </td>
-                        <td class="border py-2 md:px-2 text-center">{{$s->name}}</td>
-                        <td class="border py-2 md:px-2 text-center">{{$s->username}}</td>
+                        <td class="border py-2 md:px-2 text-center">
+                            <form action="{{ route('students.show', $s) }}" method="GET">
+                                @csrf
+                                <button type="submit" dusk="student_records" class="hover:underline">
+                                    {{ $s->name}}
+                                </button>
+                            </form>    
+                        </td>
                         <td class="border py-2 md:px-2 text-center hover:underline">
-                            <a href="mailto:{{$s->email}}" class="hover:underline">{{$s->email}}</a>
+                            <a href="mailto:{{$s->email}}" class="hover:underline">{{$s->username}}</a>
                         </td>
                         <td class="border py-2 md:px-2 text-center">
                             @if ($s->github)
-                            <a class="hover:underline" href="http://github.com/{{ $s->github }}"
-                                target="_blank">github.com/{{ $s->github }}
+                            <a class="hover:underline" href="http://www.github.com/{{ $s->github }}"
+                                target="_blank">{{ $s->github }}
                             </a>
                             @endif
                         </td>
