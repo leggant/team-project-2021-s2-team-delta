9aad144a9d2af3687e8d35758a0d7b6aebfebe5b
Anthony Legg <leggtc1@student.op.ac.nz>
2021-11-09 17:07:40 +1300

    fix(controller and route change added for student list): student list is not accessible
    have created a controller for this part of the site and update the route file so it will refer to
    this

---
+++ app/Http/Controllers/StudentListController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+use App\Models\Cohort;
+
+class StudentListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::orderBy('is_active', 'desc')->get();
+        $cohorts = Cohort::all();
+        $groups = $students->groupBy('is_active');
+        $user = auth()->user();
+        return view(
+            'pages.studentListView',
+            compact('students', 'user', 'groups', 'cohorts')
+        );
+    }
+}
